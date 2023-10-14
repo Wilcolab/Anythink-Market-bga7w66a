78a30ce102d003e36dbbb802a0bb07ef6b7276eb
329henry <42580331+329henry@users.noreply.github.com>
2023-10-14 17:18:21 +0000

    edit seed script for backend

--- backend/scripts/seeds.js
+++ backend/scripts/seeds.js
@@ -1,1 +1,47 @@
-//TODO: seeds script should come here, so we'll be able to put some data in our local env
+const mongoose = require("mongoose");
+const connection = process.env.MONGODB_URI;
+mongoose.connect(connection);
+
+const User = mongoose.model("User");
+const Item = mongoose.model("Item");
+const Comment = mongoose.model("Comment");
+
+async function seedDatabase() {
+    for(let i = 0; i < 100; i++) {
+        const user = { username: `user${i}`, email: `user${i}@gmail.com`};
+        const options = { upsert: true, new: true};
+        const createdUser = await User.findOneAndUpdate(user, {}, options);
+
+        const item = {
+            slug: `slug${i}`,
+            title: `title${i}`,
+            description: `description${i}`,
+            seller: createdUser,
+        }
+        const createdItem = await Item.findOneAndUpdate(item, {}, options);
+
+        if (!createdItem?.comments?.length) {
+            let commentIds = [];
+            for(let j = 0; j < 100; j++) {
+                const comment = new Comment({
+                    body: `body ${j}`,
+                    seller: createdUser,
+                    item: createdItem,
+                });
+
+                await comment.saved();
+                commentIds.push(comment._id);
+            }
+            createdItem.comments = commentIds;
+            await createdItem.save();
+        }
+    }
+}
+
+seedDatabase().then(() => {
+    console.log('Finished DB seeding');
+    process.exit(0);
+}).catch((err) => {
+    console.log(`Error while running DB seed: ${err.message}`);
+    process.exit(1);
+})
